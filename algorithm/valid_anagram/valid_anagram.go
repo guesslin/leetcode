1098599c65b6592c7b777d98005b658f49d0fa94
Yu-Han Lin <guesslin1986@gmail.com>
2016-04-07 15:38:27 +0800

    Update: Algorithm 242. Valid Anagram with improve speed from 24ms to 8ms
    	1. 增加條件判斷
    		若是兩個輸入字串都為空字串則回傳 true
    		在以上條件之後，若其中一個字串為空字串則回傳 false
    	2. 改用 var dict [26]int 陣列來存放字典

--- algorithm/valid_anagram/valid_anagram.go
+++ algorithm/valid_anagram/valid_anagram.go
@@ -5,12 +5,17 @@ import (
 )
 
 func isAnagram(s string, t string) bool {
-	dict := make(map[rune]int)
+	if s == "" && t == "" {
+		return true
+	} else if s == "" || t == "" {
+		return false
+	}
+	var dict [26]int
 	for _, c := range s {
-		dict[c] += 1
+		dict[c-97] += 1
 	}
 	for _, c := range t {
-		dict[c] -= 1
+		dict[c-97] -= 1
 	}
 	for _, i := range dict {
 		if i != 0 {
@@ -23,4 +28,5 @@ func isAnagram(s string, t string) bool {
 func main() {
 	fmt.Println(isAnagram("anagram", "nagaram"))
 	fmt.Println(isAnagram("rat", "car"))
+	fmt.Println(isAnagram("", ""))
 }
