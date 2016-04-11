14a0fdd3fe3827a950bc0114da888226fc6f991d
Yu-Han Lin <guesslin1986@gmail.com>
2016-04-11 14:38:13 +0800

    Update: Algorithm 50. Pow(x, n) with shorter code but 8ms
    	速度變慢了 6ms -> 8ms

--- algorithm/pow/pow.go
+++ algorithm/pow/pow.go
@@ -13,20 +13,22 @@ func myPow(x float64, n int) float64 {
 		flag = -1
 		n = -n
 	}
+	result := myPow(x*x, n/2)
 	if n/2*2 != n {
-		if flag == -1 {
-			return 1 / (myPow(x*x, n/2) * x)
-		}
-		return myPow(x*x, n/2) * x
+		result = result * x
 	}
 	if flag == -1 {
-		return 1 / myPow(x*x, n/2)
+		return 1 / result
 	}
-	return myPow(x*x, n/2)
+	return result
 }
 
 func main() {
 	fmt.Println(myPow(2, 1))
+	fmt.Println(myPow(2, 0))
 	fmt.Println(myPow(2, 14))
+	fmt.Println(myPow(0.1, -2))
+	fmt.Println(myPow(0.1, 2))
+	fmt.Println(myPow(8.88023, 3))
 	fmt.Println(myPow(2, -2))
 }
