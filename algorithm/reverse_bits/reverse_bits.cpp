13ea61cab1c02655386d43110603e0a9d29f721f
Yu-Han Lin <guesslin1986@gmail.com>
2016-04-15 15:22:23 +0800

    Update: Algorithm 190. Reverse Bits
    	建表查詢 一次掃 4 bits

--- algorithm/reverse_bits/reverse_bits.cpp
+++ algorithm/reverse_bits/reverse_bits.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <cstdint>
 
+
+unsigned int bits[] = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+
 class Solution {
 	public:
 		uint32_t reverseBits(uint32_t n) {
 			uint32_t r = 0;
-			for (int i = 0; i < 32; i++) {
-				r = r<<1;
-				r+=n&1;
-				n = n>>1;
+			for (int i = 0; i < 8; i++) {
+				r = r<<4;
+				r+=bits[n&15];
+				n = n>>4;
 			}
 			return r;
 		}
