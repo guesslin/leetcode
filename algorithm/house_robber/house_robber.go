80d469d36d783fdc2aa197865394db6b281d8d3d
Yu-Han Lin <guesslin1986@gmail.com>
2016-04-22 11:35:35 +0800

    Add: Algorithm 198. House Robber
    	列一個矩陣 count 長度為 len(nums)
    	令
    		count[0] = nums[0]
    		count[1] = max(nums[0], nums[1])
    		* i >= 2
    		count[i] = max(count[i-1], nums[i] + count[i-2])
    	最後回傳 count 最後一項

---
+++ algorithm/house_robber/house_robber.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rob(nums []int) int {
+	houses := len(nums)
+	if houses == 0 {
+		return 0
+	}
+	if houses == 1 {
+		return nums[0]
+	}
+	count := make([]int, houses+1)
+	count[0] = nums[0]
+	count[1] = max(nums[0], nums[1])
+	for i := 2; i < houses; i++ {
+		count[i] = max(count[i-1], nums[i]+count[i-2])
+	}
+	return count[houses-1]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	fmt.Println(rob([]int{1, 2, 3, 4, 5, 6}))
+	fmt.Println(rob([]int{0}))
+	fmt.Println(rob([]int{2, 1}))
+	fmt.Println(rob([]int{2, 6, 1}))
+}
