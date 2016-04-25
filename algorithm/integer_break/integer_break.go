4382f0bb04671ec8c24ffcb637929df35ccd3ffe
Yu-Han Lin <guesslin1986@gmail.com>
2016-04-25 12:21:25 +0800

    Update: Algorithm 343. Integer Break
    	更新判斷條件以及pow函式，借用50 Pow(x, n)的myPow並修改簡化條件

--- algorithm/integer_break/integer_break.go
+++ algorithm/integer_break/integer_break.go
@@ -11,29 +11,30 @@ func integerBreak(n int) int {
 	if n == 3 {
 		return 2
 	}
-	if n == 4 {
-		return 4
-	}
 	x := n / 3
-	if n%3 == 0 {
+	remainder := n % 3
+	if remainder == 0 {
 		return pow(3, x)
-	}
-	if n%3 == 1 {
+	} else if remainder == 1 {
 		return pow(3, x-1) * 4
+	} else {
+		return pow(3, x) * 2
 	}
-	return pow(3, x) * 2
 }
 
-func pow(x, p int) int {
-	r := x
-	for i := 1; i < p; i++ {
-		r *= x
+func pow(x, n int) int {
+	if n == 0 {
+		return 1
 	}
-	return r
+	result := pow(x*x, n/2)
+	if n/2*2 != n {
+		result = result * x
+	}
+	return result
 }
 
 func main() {
 	for i := 2; i <= 13; i++ {
-		fmt.Println(integerBreak(i))
+		fmt.Printf("Number %d can break into max product %d\n", i, integerBreak(i))
 	}
 }
