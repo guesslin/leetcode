6377dc7ccfb35285f99cde9dc545037568ed983b
Yu-Han Lin <guesslin1986@gmail.com>
2016-08-23 16:55:05 +0800

    Update: Algorithm 387. First Unique Character in a String with go 36 ms
    	用一個 list 存每個字元出現第一次的位置，然後當字元出現一次以上就
    	把該字元的元素從上述 list 中剔除，最後檢查該 list 是否為空，
    	若為空，則回傳 -1，否則回傳 list 第一個元素所指到的 index

--- algorithm/first_unique_character_in_a_string/first_unique_character_in_a_string.go
+++ algorithm/first_unique_character_in_a_string/first_unique_character_in_a_string.go
@@ -4,25 +4,36 @@ import (
 	"fmt"
 )
 
+type Uniq struct {
+	Char  byte
+	Index int
+}
+
 func firstUniqChar(s string) int {
 	if len(s) == 0 {
 		return -1
 	}
 	words := make([]int, 26)
-	slow := 0
+	first := make([]Uniq, 0, 26)
 	n := len(s)
 	for i := 0; i < n; i++ {
 		w := int(s[i] - 'a')
 		words[w]++
-		if slow < n && words[int(s[slow]-'a')] > 1 {
-			for ; slow < n && words[int(s[slow]-'a')] > 1; slow++ {
+		if words[w] == 1 {
+			first = append(first, Uniq{Char: s[i], Index: i})
+		} else {
+			for f, unit := range first {
+				if unit.Char == s[i] {
+					first = append(first[:f], first[f+1:]...)
+					break
+				}
 			}
 		}
 	}
-	if slow >= n {
-		return -1
+	if len(first) != 0 {
+		return first[0].Index
 	}
-	return slow
+	return -1
 }
 
 func main() {
