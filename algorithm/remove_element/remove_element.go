dcf86a9ec692654ed1e7d9da2f319db369fb64f8
Yu-Han Lin <guesslin1986@gmail.com>
2016-08-24 16:44:22 +0800

    Update: Algorithm 27. Remove Element with go 4 ms
    	改用 two pointer 來做 element 交換，把 val 都換去 list 的末端

--- algorithm/remove_element/remove_element.go
+++ algorithm/remove_element/remove_element.go
@@ -5,18 +5,19 @@ import (
 )
 
 func removeElement(nums []int, val int) int {
-	for i := 0; i < len(nums); {
-		if nums[i] == val {
-			if i == len(nums)-1 {
-				nums = nums[:i]
-			} else {
-				nums = append(nums[:i], nums[i+1:]...)
-			}
-		} else {
+	i, j := 0, len(nums)-1
+	for i <= j {
+		for i < len(nums) && nums[i] != val {
 			i++
 		}
+		for j >= 0 && nums[j] == val {
+			j--
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
 	}
-	return len(nums)
+	return j + 1
 }
 
 func main() {
@@ -26,10 +27,12 @@ func main() {
 		expcet int
 	}{
 		{nums: []int{3, 2, 2, 3}, val: 3, expcet: 2},
+		{nums: []int{1}, val: 1, expcet: 0},
+		{nums: []int{}, val: 1, expcet: 0},
 	}
 	for _, c := range testCases {
 		fmt.Printf("Nums: %v\n", c.nums)
 		res := removeElement(c.nums, c.val)
-		fmt.Printf("Nums: %v, Len: %d\n", c.nums, res)
+		fmt.Printf("Nums: %v, Len: %d, Same: %v\n", c.nums, res, res == c.expcet)
 	}
 }
