12b739c898390cf2c22edb36d2c4d7cc1469f9d6
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-04 12:25:46 +0800

    Update: Algorithm 228. Summary Ranges with go in 0 ms
    	改用 start -> end 做儲存，空間複雜度降到
    	O(m) /* 多少連續數字間隔 */ + O(2) /* 存開頭跟結尾 */

--- algorithm/summary_ranges/summary_ranges.go
+++ algorithm/summary_ranges/summary_ranges.go
@@ -9,30 +9,29 @@ func summaryRanges(nums []int) []string {
 		return nil
 	}
 	res := make([]string, 0)
-	rang := make([]int, 2)
-	rang[0] = nums[0]
-	rang[1] = nums[0]
+	start, end := nums[0], nums[0]
 	for i := 1; i < len(nums); i++ {
-		if nums[i] == rang[len(rang)-1]+1 {
-			rang[len(rang)-1] = nums[i]
+		if nums[i] == end+1 {
+			end = nums[i]
 		} else {
-			rang = append(rang, nums[i])
-			rang = append(rang, nums[i])
+			if start != end {
+				res = append(res, fmt.Sprintf("%d->%d", start, end))
+			} else {
+				res = append(res, fmt.Sprintf("%d", start))
+			}
+			start, end = nums[i], nums[i]
 		}
 	}
-	for i := 0; i < len(rang); i += 2 {
-		if rang[i] != rang[i+1] {
-			res = append(res, fmt.Sprintf("%d->%d", rang[i], rang[i+1]))
-		} else {
-			res = append(res, fmt.Sprintf("%d", rang[i]))
-		}
+	if start != end {
+		res = append(res, fmt.Sprintf("%d->%d", start, end))
+	} else {
+		res = append(res, fmt.Sprintf("%d", start))
 	}
-
 	return res
 }
 
 func main() {
-	// nums := []int{0, 1, 2, 4, 5, 7, 8, 9, 11}
-	nums := []int{1, 3}
+	nums := []int{0, 1, 2, 4, 5, 7, 8, 9}
+	// nums := []int{1, 3}
 	fmt.Println(summaryRanges(nums))
 }
