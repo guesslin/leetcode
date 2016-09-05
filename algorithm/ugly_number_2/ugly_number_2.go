999f64581072bc9141aa6f2ad2d8ba025d2caa85
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-05 15:42:15 +0800

    Update: Algorithm 264. Ugly Number II with go in 85 ms

--- algorithm/ugly_number_2/ugly_number_2.go
+++ algorithm/ugly_number_2/ugly_number_2.go
@@ -5,37 +5,36 @@ import (
 )
 
 func nthUglyNumber(n int) int {
-	m2, m3, m5 := 2, 3, 5
-	var id2, id3, id5 int
+	primes := []int{2, 3, 5}
+	m := []int{2, 3, 5}
+	index := []int{0, 0, 0}
 	seq := make([]int, n, n)
 	seq[0] = 1
 	for i := 1; i < n; i++ {
-		minV := min(min(m2, m3), m5)
+		minV := min(m)
 		seq[i] = minV
-		if minV == m2 {
-			id2++
-			m2 = 2 * seq[id2]
-		}
-		if minV == m3 {
-			id3++
-			m3 = 3 * seq[id3]
-		}
-		if minV == m5 {
-			id5++
-			m5 = 5 * seq[id5]
+		for j := 0; j < len(index); j++ {
+			if minV == m[j] {
+				index[j]++
+				m[j] = primes[j] * seq[index[j]]
+			}
 		}
 	}
+	fmt.Println(seq)
 	return seq[n-1]
 }
 
-func min(x, y int) int {
-	if x > y {
-		return y
+func min(nums []int) int {
+	m := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if m > nums[i] {
+			m = nums[i]
+		}
 	}
-	return x
+	return m
 }
 
 func main() {
-	n := 1500
+	n := 7
 	fmt.Printf("%d-th Ugly Number is %d\n", n, nthUglyNumber(n))
 }
