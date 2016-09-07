55c28818613adbfaf0624fff6cbbfcd43001fb18
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-07 09:48:10 +0800

    Update: Algorithm 34. Search for a Range with go in 19 ms

--- algorithm/search_for_a_range/search_for_a_range.go
+++ algorithm/search_for_a_range/search_for_a_range.go
@@ -4,34 +4,47 @@ import (
 	"fmt"
 )
 
+// UpperBound return the number of element is smaller or equal than target in list
+func UpperBound(nums []int, target int) int {
+	s, e := 0, len(nums)-1
+	for s <= e {
+		mid := (s + e) / 2
+		if nums[mid] > target {
+			e = mid - 1
+		} else {
+			s = mid + 1
+		}
+	}
+	if 0 <= e && nums[e] == target {
+		return e
+	}
+	return -1
+}
+
+// LowerBound return the number of element is smaller than target in list
+func LowerBound(nums []int, target int) int {
+	s, e := 0, len(nums)-1
+	for s <= e {
+		mid := (s + e) / 2
+		if nums[mid] >= target {
+			e = mid - 1
+		} else {
+			s = mid + 1
+		}
+	}
+	if s < len(nums) && nums[s] == target {
+		return s
+	}
+	return -1
+}
+
 func searchRange(nums []int, target int) []int {
-	notFound := []int{-1, -1}
-	l, r := 0, len(nums)-1
-	for l < r {
-		m := (l+r)/2 + 1
-		if nums[m] > target {
-			r = m - 1
-		} else {
-			l = m
-		}
-	}
-	if nums[r] != target {
-		return notFound
-	}
-	right := r
-	l = 0
-	for l < r {
-		m := (l + r) / 2
-		if nums[m] < target {
-			l = m + 1
-		} else {
-			r = m
-		}
-	}
-	return []int{l, right}
+	l, r := LowerBound(nums, target), UpperBound(nums, target)
+	return []int{l, r}
 }
 
 func main() {
+	fmt.Println(searchRange([]int{1}, 0), false)
 	fmt.Println(searchRange([]int{1, 4}, 4), true)
 	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 8, 10}, 8), true)
 	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 10}, 6), false)
