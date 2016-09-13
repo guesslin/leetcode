37b81704ccf2d1c106f46a7037c6a58851c61394
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-13 15:50:18 +0800

    Update: Algorithm 347. Top K Frequent Elements with go in 55 ms

--- algorithm/top_k_frequent_elements/top_k_frequent_elements.go
+++ algorithm/top_k_frequent_elements/top_k_frequent_elements.go
@@ -5,53 +5,25 @@ import (
 	"reflect"
 )
 
-type HeapNode struct {
-	Val, Count int
-}
-
 func topKFrequent(nums []int, k int) []int {
 	record := make(map[int]int)
-	heap := make([]HeapNode, 0)
 	for _, c := range nums {
-		if i, ok := record[c]; ok {
-			heap[i].Count++
-			adjustRecordHeap(record, heap)
-		} else {
-			heap = append(heap, HeapNode{Val: c, Count: 1})
-			record[c] = len(heap) - 1
-			adjustRecordHeap(record, heap)
-		}
+		record[c]++
 	}
-	fmt.Println("Heap", heap)
-	fmt.Println("Record", record)
+	n := len(nums)
+	freq := make([][]int, len(nums)+1)
 	res := make([]int, 0, k)
-	for i := 0; i < k; i++ {
-		res = append(res, heap[0].Val)
-		delete(record, heap[0].Val)
-		heap = heap[1:]
-		adjustRecordHeap(record, heap)
+	for k, v := range record {
+		freq[v] = append(freq[v], k)
 	}
-	return res
-}
-
-func adjustRecordHeap(record map[int]int, heap []HeapNode) {
-	root := 0
-	for {
-		child := root*2 + 1
-		if child >= len(heap) {
-			return
+	for i := n; i >= 0; i-- {
+		res = append(res, freq[i]...)
+		if len(res) >= k {
+			break
 		}
-		if child+1 < len(heap) && heap[child].Count < heap[child+1].Count {
-			child++
-		}
-		if heap[root].Count >= heap[child].Count {
-			root++
-			continue
-		}
-		record[heap[root].Val], record[heap[child].Val] = record[heap[child].Val], record[heap[root].Val]
-		heap[root].Val, heap[root].Count, heap[child].Val, heap[child].Count = heap[child].Val, heap[child].Count, heap[root].Val, heap[root].Count
-		root = child
 	}
+	fmt.Println(res)
+	return res[:k]
 }
 
 func main() {
@@ -60,6 +32,7 @@ func main() {
 		k      int
 		expect []int
 	}{
+		{nums: []int{1}, k: 1, expect: []int{1}},
 		{nums: []int{1, 1, 1, 2, 2, 3}, k: 2, expect: []int{1, 2}},
 		{nums: []int{2, 3, 4, 1, 4, 0, 4, -1, -2, -1}, k: 2, expect: []int{4, -1}},
 	}
@@ -67,7 +40,7 @@ func main() {
 		res := topKFrequent(c.nums, c.k)
 		eq := reflect.DeepEqual(res, c.expect)
 		if !eq {
-			fmt.Println(i, res, c.expect, c.k, c.nums)
+			fmt.Println(i, "res", res, "expect", c.expect, c.k, c.nums)
 		}
 	}
 
