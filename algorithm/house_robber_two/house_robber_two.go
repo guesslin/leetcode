c3db5b1ffa453a7fb2923319bc23d54044e1e7c6
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-16 19:51:14 +0800

    Update: Algorithm 213. House Robber II with go in 3 ms
    	直接設定兩個陣列，一個有選第一間房子另一個沒選第一間房子
    	有選第一間房子的最大值在倒數第二間的位置
    	比較這兩個的最大值回傳

--- algorithm/house_robber_two/house_robber_two.go
+++ algorithm/house_robber_two/house_robber_two.go
@@ -14,72 +14,34 @@ func rob(nums []int) int {
 	if houses == 1 {
 		return nums[0]
 	}
-	headSelected := make([]bool, houses)
-	lastSelected := false
-	count := make([]int, houses)
-	count[0] = nums[0]
-	headSelected[0] = true
-	if nums[0] > nums[1] {
-		count[1] = nums[0]
-		headSelected[1] = headSelected[0]
-	} else {
-		count[1] = nums[1]
-	}
+	first := make([]int, houses)
+	notFirst := make([]int, houses)
+	first[0] = nums[0]
+	first[1] = max(nums[0], nums[1])
+	notFirst[0] = 0
+	notFirst[1] = nums[1]
 	for i := 2; i < houses; i++ {
-		if count[i-1] > nums[i]+count[i-2] {
-			count[i] = count[i-1]
-			headSelected[i] = headSelected[i-1]
-			lastSelected = false
-		} else {
-			count[i] = nums[i] + count[i-2]
-			headSelected[i] = headSelected[i-2]
-			lastSelected = true
-		}
+		first[i] = max(first[i-1], nums[i]+first[i-2])
+		notFirst[i] = max(notFirst[i-1], nums[i]+notFirst[i-2])
 	}
-	// 頭尾沒有相鄰
-	if !headSelected[houses-1] || !lastSelected {
-		return count[houses-1]
+	return max(first[houses-2], notFirst[houses-1])
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
 	}
-	// 頭尾有相鄰，找減去頭或尾之後哪個數字大
-	// 還要注意跟最後一個數字相比較
-	if nums[0] > nums[houses-1] {
-		count[houses-1] = count[houses-1] - nums[houses-1]
-	} else {
-		count[houses-1] = count[houses-1] - nums[0]
-	}
-	lastMax := 0
-	if count[houses-1] > count[houses-2] {
-		lastMax = count[houses-1]
-	} else {
-		lastMax = count[houses-2]
-	}
-	count[1] = nums[1]
-	if nums[1] > nums[2] {
-		count[2] = nums[1]
-	} else {
-		count[2] = nums[2]
-	}
-	for i := 3; i < houses; i++ {
-		if count[i-1] > nums[i]+count[i-2] {
-			count[i] = count[i-1]
-		} else {
-			count[i] = nums[i] + count[i-2]
-		}
-	}
-	if lastMax > count[houses-1] {
-		return lastMax
-	}
-	return count[houses-1]
+	return y
 }
 
 func main() {
-	//	fmt.Println(rob([]int{1, 2, 3, 4, 5, 6}))
+	fmt.Println(rob([]int{1, 2, 3, 4, 5, 6}))
 	fmt.Println(rob([]int{2, 2, 4, 3, 2, 5}))
-	//	fmt.Println(rob([]int{2, 1}))
-	//	fmt.Println(rob([]int{2, 3, 2}))
-	//	fmt.Println(rob([]int{}))
-	//	fmt.Println(rob([]int{1, 2, 3, 4, 5, 6, 7}))
-	//	fmt.Println(rob([]int{0}))
-	//	fmt.Println(rob([]int{2, 1}))
-	//	fmt.Println(rob([]int{2, 6, 1}))
+	fmt.Println(rob([]int{2, 1}))
+	fmt.Println(rob([]int{2, 3, 2}))
+	fmt.Println(rob([]int{}))
+	fmt.Println(rob([]int{1, 2, 3, 4, 5, 6, 7}))
+	fmt.Println(rob([]int{0}))
+	fmt.Println(rob([]int{2, 1}))
+	fmt.Println(rob([]int{2, 6, 1}))
 }
