5ee582b6112e04190b757b94068ca8310ce70df1
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-25 17:59:58 +0800

    Update: Algorithm 1. Two Sum with go in 9 ms
    	single pass with hash table

--- algorithm/two_sum/two_sum.go
+++ algorithm/two_sum/two_sum.go
@@ -5,22 +5,13 @@ import (
 )
 
 func twoSum(nums []int, target int) []int {
-	record := make(map[int][]int)
+	records := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		if _, ok := record[nums[i]]; ok {
-			record[nums[i]] = append(record[nums[i]], i)
-		} else {
-			record[nums[i]] = []int{i}
+		search := target - nums[i]
+		if j, ok := records[search]; ok {
+			return []int{j, i}
 		}
-	}
-	for i := 0; i < len(nums); i++ {
-		if j, ok := record[target-nums[i]]; ok {
-			for _, c := range j {
-				if c != i {
-					return []int{i, c}
-				}
-			}
-		}
+		records[nums[i]] = i
 	}
 	return nil
 }
