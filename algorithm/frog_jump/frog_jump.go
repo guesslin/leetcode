7e6af2da90330c3ed2f5197a444c978132df9101
Yu-Han Lin <guesslin1986@gmail.com>
2016-09-29 15:23:36 +0800

    Add: Algorithm 403. Frog Jump with go in 179 ms
    	use DP which Time complexity is O(N^2)
    	with maxStone filter time goes to 169 ms

---
+++ algorithm/frog_jump/frog_jump.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func canCross(stones []int) bool {
+	// start stone is always sit on zero, and it will always be jumpable
+	if len(stones) == 1 {
+		return true
+	}
+	stonesMap := make(map[int]map[int]bool)
+	for _, p := range stones {
+		stonesMap[p] = make(map[int]bool)
+	}
+	// if the first jump can't jump to the second stone, then it will always be false
+	if _, ok := stonesMap[1]; !ok {
+		return false
+	}
+	maxStone := 1
+	stonesMap[1][1] = true
+	for i := 1; i < len(stones); i++ {
+		if i > maxStone {
+			break
+		}
+		for k, _ := range stonesMap[stones[i]] {
+			if _, ok := stonesMap[stones[i]+k-1]; ok && (k-1) > 0 {
+				stonesMap[stones[i]+k-1][k-1] = true
+			}
+			if _, ok := stonesMap[stones[i]+k]; ok {
+				stonesMap[stones[i]+k][k] = true
+			}
+			if _, ok := stonesMap[stones[i]+k+1]; ok {
+				stonesMap[stones[i]+k+1][k+1] = true
+			}
+			if stones[i]+k+1 > maxStone {
+				maxStone = stones[i] + k + 1
+			}
+		}
+	}
+	return len(stonesMap[stones[len(stones)-1]]) > 0
+}
+
+func main() {
+	stones := []int{0, 1, 3, 4, 5, 7, 9, 10, 12}
+	fmt.Println(stones)
+	if canCross(stones) {
+		fmt.Println("Frog can cross the river!")
+	} else {
+		fmt.Println("Frog can NOT cross the river!")
+	}
+}
