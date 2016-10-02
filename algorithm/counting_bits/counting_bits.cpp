958b79264eddd68959a954a82cf089a52b417122
Yu-Han Lin <guesslin1986@gmail.com>
2016-10-02 20:13:27 +0800

    Update: Algorithm 338. Counting Bits with cpp in 72 ms

--- algorithm/counting_bits/counting_bits.cpp
+++ algorithm/counting_bits/counting_bits.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 
 using std::vector;
+using std::cout;
+using std::endl;
 
 class Solution {
 public:
 	vector<int> countBits(int num) {
-		vector<int> r;
-		r.push_back(0);
+		vector<int> r(num+1, 0);
 		if (num == 0) {
 			return r;
 		}
-		for (int i = 0; i <= num; ++i) {
-			r.push_back(r[i&(i-1)]+1);
+		for (int i = 1; i <= num; ++i) {
+			r[i] = r[i&(i-1)]+1;
 		}
 		return r;
 
 	}
 };
 
-int main(int argc, char **argv) {
+int main(void) {
 	Solution s;
 	vector<int> result = s.countBits(123456);
 	for (auto ite:result) {
 		std::cout<<ite<<" ";
 	}
-	std::cout<<std::endl;
+	cout << endl;
 	return 0;
 }
 
