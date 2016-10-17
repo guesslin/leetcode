40caafbfecd94aeb8fe0e5a73203159c6e900932
Yu-Han Lin <guesslin1986@gmail.com>
2016-10-17 16:50:18 +0800

    Update: Codility Counting Elements MaxCounters with go

--- algorithm/max_counters/max_counters.go
+++ algorithm/max_counters/max_counters.go
@@ -10,18 +10,25 @@ package solution
 func Solution(N int, A []int) []int {
 	// write your code in Go 1.4
 	res := make([]int, N)
-	curMax := 0
+	curMax, curBase := 0, 0
 	// maxCounter = N+1
 	for _, op := range A {
 		if op <= N {
-			res[op-1]++
+			if res[op-1] >= curBase {
+				res[op-1]++
+			} else {
+				res[op-1] = curBase + 1
+			}
 			if res[op-1] > curMax {
 				curMax = res[op-1]
 			}
 		} else {
-			for i := range res {
-				res[i] = curMax
-			}
+			curBase = curMax
+		}
+	}
+	for i := 0; i < N; i++ {
+		if res[i] < curBase {
+			res[i] = curBase
 		}
 	}
 	return res
