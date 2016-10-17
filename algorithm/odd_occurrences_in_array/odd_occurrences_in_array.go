58aab7a51171dc177addbd743c56ae18fd3457d1
Yu-Han Lin <guesslin1986@gmail.com>
2016-10-17 10:09:54 +0800

    Add: Codility Arrays OddOccurrencesInArray with go

---
+++ algorithm/odd_occurrences_in_array/odd_occurrences_in_array.go
@@ -0,0 +1,17 @@
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+	// write your code in Go 1.4
+	res := 0
+	for _, c := range A {
+		res = res ^ c
+	}
+	return res
+}
