308af7291ca2b932ac4d4bd4c5a4ef0d962df06b
Yu-Han Lin <guesslin1986@gmail.com>
2022-08-15 13:49:09 +0800

    Add: 419 Battleships in a board. Second solution

--- algorithm/battleships_in_a_board/battleships_in_a_board.go
+++ algorithm/battleships_in_a_board/battleships_in_a_board.go
@@ -19,3 +19,30 @@ func countBattleships(board [][]byte) int {
 	}
 	return ships
 }
+
+func countBattleships2(board [][]byte) int {
+	ships := 0
+	for x := 0; x < len(board); x++ {
+		for y := 0; y < len(board[x]); y++ {
+			if board[x][y] == 'X' {
+				ships++
+				// fill x direction 'X's to '.'
+				for p := x + 1; p < len(board); p++ {
+					if board[p][y] == '.' {
+						break
+					}
+					board[p][y] = '.'
+
+				}
+				// fill y directino 'X's to '.'
+				for p := y + 1; p < len(board[x]); p++ {
+					if board[x][p] == '.' {
+						break
+					}
+					board[x][p] = '.'
+				}
+			}
+		}
+	}
+	return ships
+}
